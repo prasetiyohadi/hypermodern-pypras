40231a8d4dfad1c6df091a1bc1742cd9901f8a0a
Prasetiyo Hadi Purwoko <pras@deuterion.net>
2020-06-27 12:36:50 +0700

    Fixing missing newlines

--- noxfile.py
+++ noxfile.py
@@ -36,7 +36,7 @@ def lint(session):
         "flake8-bandit",
         "flake8-black",
         "flake8-bugbear",
-        "flake8-import-order"
+        "flake8-import-order",
     )
     session.run("flake8", *args)
 
