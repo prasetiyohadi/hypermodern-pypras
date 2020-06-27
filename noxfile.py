b07d478fc159b4123f00f650727f269d2c0914c4
Prasetiyo Hadi Purwoko <pras@deuterion.net>
2020-06-27 11:37:06 +0700

    Create safety nox session to run safety

--- noxfile.py
+++ noxfile.py
@@ -1,3 +1,5 @@
+import tempfile
+
 import nox
 
 
@@ -23,6 +25,22 @@ def lint(session):
         "flake8-import-order"
     )
     session.run("flake8", *args)
+
+
+@nox.session(python="3.8")
+def safety(session):
+    with tempfile.NamedTemporaryFile() as requirements:
+        session.run(
+            "poetry",
+            "export",
+            "--dev",
+            "--format=requirements.txt",
+            "--without-hashes",
+            f"--output={requirements.name}",
+            external=True,
+        )
+        session.install("safety")
+        session.run("safety", "check", f"--file={requirements.name}", "--full-report")
 
 
 @nox.session(python=["3.8", "3.7"])
