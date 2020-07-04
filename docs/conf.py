f5dbc4884a356ed9971b8cc56e2a07a17528852a
Prasetiyo Hadi Purwoko <pras@deuterion.net>
2020-07-04 19:09:32 +0700

    Enable autodoc and napoleon Sphinx extensions

--- docs/conf.py
+++ docs/conf.py
@@ -2,3 +2,8 @@
 project = "hypermodern-pypras"
 author = "Prasetiyo Hadi Purwoko"
 copyright = f"2020, {author}"
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx_autodoc_typehints",
+]
