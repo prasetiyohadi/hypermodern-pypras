73bfdc4b73c76b27963180f1a7876f19b35aeeff
Prasetiyo Hadi Purwoko <pras@deuterion.net>
2020-07-07 17:31:41 +0700

    Use importlib.metadata to declare version

--- src/hypermodern_pypras/__init__.py
+++ src/hypermodern_pypras/__init__.py
@@ -1,2 +1,11 @@
-"""The hypermodern Python project."""
-__version__ = "0.1.0"
+"""The hypermodern Pypras project."""
+try:
+    from importlib.metadata import version, PackageNotFoundError
+except ImportError:
+    from importlib_metadata import version, PackageNotFoundError
+
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "unknown"
